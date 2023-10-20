58c5805d6ee6b9b8d3bde81ac305bb33a089144e
navyasachdev03 <105008906+navyasachdev03@users.noreply.github.com>
2023-10-20 12:52:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import './App.css';
 import Homepage from './Homepage';
 import Navbar from './Navbar';
@@ -6,16 +6,36 @@ import About from './About';
 import Catalog from './Catalog';
 import Order from './Order';
 import Footer from './Footer';
+import Account from './Account';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const homeRef = useRef(null);
+
+  const handleLogin = (username) => {
+    setUser(username);
+    if (homeRef.current) {
+      homeRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+    if (homeRef.current) {
+      homeRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <Homepage />
+        <Account onLogin={handleLogin} onLogout={handleLogout}/>
+        <div ref={homeRef}>
+        {user ? <Homepage user={user} /> : <Homepage />}
+        </div>
         <About />
         <Catalog />
-        <Order />
       </div>
       <Footer />
     </div>
@@ -23,3 +43,4 @@ function App() {
 }
 
 export default App;
+
