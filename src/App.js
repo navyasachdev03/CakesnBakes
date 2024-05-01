c2eee6d290bc4facad0c069d99ee527c10b06c17
navyasachdev03 <105008906+navyasachdev03@users.noreply.github.com>
2024-05-01 23:09:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,36 +7,52 @@ import Catalog from './Catalog';
 import Order from './Order';
 import Footer from './Footer';
 import Account from './Account';
+import Cart from './Cart';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [cartItems, setCartItems] = useState([]);
   const homeRef = useRef(null);
+  const navigate = useNavigate();
 
   const handleLogin = (username) => {
     setUser(username);
-    if (homeRef.current) {
-      homeRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
+    // if (homeRef.current) {
+    //   homeRef.current.scrollIntoView({ behavior: 'smooth' });
+    // }
+    navigate('/');
   };
 
   const handleLogout = () => {
     setUser(null);
-    if (homeRef.current) {
-      homeRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
+    // if (homeRef.current) {
+    //   homeRef.current.scrollIntoView({ behavior: 'smooth' });
+    // }
+    navigate('/');
+  };
+
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  const removeFromCart = (item) => {
+    const updatedCart = cartItems.filter((cartItem) => cartItem.id !== item.id);
+    setCartItems(updatedCart);
   };
 
   return (
     <div className="App">
-      <Navbar />
+      <Navbar /> 
       <div className="content">
-        <Account onLogin={handleLogin} onLogout={handleLogout}/>
-        <div ref={homeRef}>
-        {user ? <Homepage user={user} /> : <Homepage />}
-        </div>
-        <About />
-        <Catalog />
-        <Order />
+        <Routes>
+          <Route path="/account" element={<Account onLogin={handleLogin} onLogout={handleLogout} />} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/catalog" element={<Catalog addToCart={addToCart}/>} />
+          <Route path="/order" element={<Order/>} />
+          <Route path="/cart" element={<Cart cartItems={cartItems} removeFromCart={removeFromCart}/>} />
+          <Route exact path="/" element={<div ref={homeRef}>{user ? <Homepage user={user} /> : <Homepage />}</div>}></Route>
+        </Routes>
       </div>
       <Footer />
     </div>
@@ -44,4 +60,3 @@ function App() {
 }
 
 export default App;
-
